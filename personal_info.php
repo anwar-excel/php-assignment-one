8d416118d04d696eab569d50d1fbde78dad29e52
unknown <anwarhosen575@gmail.com>
2023-10-24 09:33:38 +0600

    personal information complete

--- personal_info.php
+++ personal_info.php
@@ -1,4 +1,14 @@
 <?php
+// Create a PHP file named personal_info.php that displays your personal information using variables and the echo //statement. Include your name, age, country, and a brief introduction.
+
+$name = "Anwar Hosen";
+$age = 26;
+$countryName = "Bangladesh";
+$detail ="I am a student, I live at Mohammadpur in Dhaka";
+
+echo "Hi I am $name ,My age is $age, I am from $countryName. $detail .Thank you";
+
+
 
 
 ?>
